66661aed8824a38cbc2c8a7a703919a15e39bf16
Argentina <argentinaokon@gmail.com>
2023-06-14 18:56:59 +0100

    Kare4Kids

---
+++ app.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+function openForm() {
+    document.getElementById("myForm") .style.display = "block";
+}
+function closeForm() {
+    document.getElementById("myForm") .style.display = "none";
+}
